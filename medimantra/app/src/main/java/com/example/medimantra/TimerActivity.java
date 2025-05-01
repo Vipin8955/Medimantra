e64f90e51265a0a17bf5026b7d23106d05153526
Mandhata08 <mandhatas05@gmail.com>
2025-05-01 14:20:17 +0530

    commit

---
+++ medimantra/app/src/main/java/com/example/medimantra/TimerActivity.java
@@ -0,0 +1,12 @@
+package com.example.medimantra;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TimerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+    }
+}
